36555b560842491372702cd4600b9efdff5d0968
r-shahid <rianashahid@gmail.com>
2026-07-27 17:53:00 -0400

    add hover animation toggle

--- scripts.js
+++ scripts.js
@@ -1,1 +1,15 @@
-console.log("connected")
+const effects = document.querySelectorAll(".card");
+function hoverToggle(){
+
+    if (effects[0].classList.contains("hover-off")){
+        effects.forEach(element => {
+            element.classList.remove("hover-off");
+        });
+    }else {
+        effects.forEach(element => {
+            element.classList.add("hover-off");
+        });
+    }
+}
+const button = document.getElementById("hover-toggle");
+button.addEventListener("click", hoverToggle);
